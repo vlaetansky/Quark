d34c91bbd2af1e9dc2f7252f262d0a3afb007bba
Vasco Lavos <vazkii@hotmail.com>
2020-10-16 20:48:12 +0100

    Improve StructurePieceMixin

--- src/main/java/vazkii/quark/base/mixin/StructurePieceMixin.java
+++ src/main/java/vazkii/quark/base/mixin/StructurePieceMixin.java
@@ -2,20 +2,23 @@ package vazkii.quark.base.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
 import vazkii.quark.base.handler.AsmHooks;
 
 @Mixin(StructurePiece.class)
 public class StructurePieceMixin {
 
-	@ModifyVariable(method = "correctFacing", at = @At("HEAD"), index = 2)
-	private static BlockState correctFacing(BlockState blockStateIn) {
-		return AsmHooks.getGenerationChestBlockState(blockStateIn);
+	@ModifyArg(
+		    method = "generateChest(Lnet/minecraft/world/IServerWorld;Lnet/minecraft/util/math/MutableBoundingBox;Ljava/util/Random;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/ResourceLocation;Lnet/minecraft/block/BlockState;)Z",
+		    at = @At(value = "INVOKE", target = "Lnet/minecraft/world/IServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"),
+		    index = 1
+		)
+	protected BlockState modifyBlockstate(BlockState state) {
+		return AsmHooks.getGenerationChestBlockState(state);
 	}
+
 	
 }
