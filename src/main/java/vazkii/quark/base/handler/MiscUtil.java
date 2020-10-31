5b55ec2cf13f88fe05bc7bb8992d6da770195666
MoriyaShiine <moriyashiine@gmail.com>
2020-10-30 20:29:57 -0500

    remove stray import

--- src/main/java/vazkii/quark/base/handler/MiscUtil.java
+++ src/main/java/vazkii/quark/base/handler/MiscUtil.java
@@ -4,7 +4,6 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
