4c079fb154378cfe0ec02c99a2419f37d1ed80a0
Vasco Lavos <vazkii@hotmail.com>
2020-11-09 08:56:30 +0000

    Quark programmer art

--- src/main/java/vazkii/quark/base/proxy/ClientProxy.java
+++ src/main/java/vazkii/quark/base/proxy/ClientProxy.java
@@ -1,5 +1,10 @@
 package vazkii.quark.base.proxy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.Month;
 
@@ -14,6 +19,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import vazkii.quark.base.Quark;
 import vazkii.quark.base.client.GoVoteHandler;
 import vazkii.quark.base.client.config.IngameConfigHandler;
 import vazkii.quark.base.client.config.gui.QuarkConfigHomeScreen;
@@ -26,7 +32,7 @@ import vazkii.quark.base.module.config.IConfigCallback;
 public class ClientProxy extends CommonProxy {
 
 	public static boolean jingleBellsMotherfucker = false;
-	
+
 	@Override
 	public void start() {
 		LocalDateTime now = LocalDateTime.now();
@@ -34,11 +40,13 @@ public class ClientProxy extends CommonProxy {
 			jingleBellsMotherfucker = true;
 
 		super.start();
-		
+
 		ModuleLoader.INSTANCE.clientStart();
 
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY,
 				() -> (minecraft, screen) -> new QuarkConfigHomeScreen(screen));
+
+		copyProgrammerArtIfMissing();
 	}
 
 	@Override
@@ -60,7 +68,7 @@ public class ClientProxy extends CommonProxy {
 	public void modelRegistry(ModelRegistryEvent event) {
 		ModuleLoader.INSTANCE.modelRegistry();
 	}
-	
+
 	public void textureStitch(TextureStitchEvent.Pre event) {
 		ModuleLoader.INSTANCE.textureStitch(event);
 	}
@@ -83,16 +91,38 @@ public class ClientProxy extends CommonProxy {
 					mc.player.sendMessage(new TranslationTextComponent("quark.misc.reloaded" + i).mergeStyle(i == 0 ? TextFormatting.AQUA : TextFormatting.WHITE), null);
 		});
 	}
-	
+
 	@Override
 	protected void initContributorRewards() {
 		ContributorRewardHandler.getLocalName();
 		super.initContributorRewards();
 	}
-	
+
 	@Override
 	public IConfigCallback getConfigCallback() {
 		return IngameConfigHandler.INSTANCE;
+	}
+
+	private static void copyProgrammerArtIfMissing() {
+		File dir = new File(".", "resourcepacks");
+		File target = new File(dir, "Quark Programmer Art.zip");
+
+		if(!target.exists()) 
+			try {
+				dir.mkdirs();
+				InputStream in = Quark.class.getResourceAsStream("/assets/quark/programmer_art.zip");
+				FileOutputStream out = new FileOutputStream(target);
+				
+				byte[] buf = new byte[16384];
+				int len = 0;
+				while((len = in.read(buf)) > 0)
+					out.write(buf, 0, len);
+
+				in.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
 
 }
