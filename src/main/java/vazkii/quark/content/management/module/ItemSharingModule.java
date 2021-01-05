090f8ee944ba1cd4468314dc3a00b5d2f9dfb192
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2021-01-04 18:28:49 -0600

    Repair item sharing (TODO: fade properly)

--- src/main/java/vazkii/quark/content/management/module/ItemSharingModule.java
+++ src/main/java/vazkii/quark/content/management/module/ItemSharingModule.java
@@ -11,6 +11,7 @@
 package vazkii.quark.content.management.module;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lwjgl.opengl.GL11;
 
@@ -37,12 +38,13 @@ import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.ServerPlayNetHandler;
 import net.minecraft.server.management.PlayerList;
-import net.minecraft.util.IReorderingProcessor;
+//import net.minecraft.util.IReorderingProcessor;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.ITextProperties;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -130,10 +132,9 @@ public class ItemSharingModule extends QuarkModule {
 			component.mergeStyle(style);
 		}
 
-//		IFormattableTextComponent out = new StringTextComponent("   ");
-//		out.func_230530_a_(style);
-//		return out.func_230529_a_(component);
-		return component;
+		IFormattableTextComponent out = new StringTextComponent("   ");
+		out.setStyle(style);
+		return out.append(component);
 	}
 
 	@OnlyIn(Dist.CLIENT)
@@ -143,121 +144,113 @@ public class ItemSharingModule extends QuarkModule {
 		chatY = event.getPosY();
 	}
 
-//	@SubscribeEvent
-//	@OnlyIn(Dist.CLIENT) // TODO wire fix this thanks
-//	public void renderSymbols(RenderGameOverlayEvent.Post event) {
-//		if (!renderItemsInChat)
-//			return;
-//
-//		Minecraft mc = Minecraft.getInstance();
-//		IngameGui gameGui = mc.ingameGUI;
-//		NewChatGui chatGui = gameGui.getChatGUI();
-//		if (event.getType() == RenderGameOverlayEvent.ElementType.CHAT) {
-//			int updateCounter = gameGui.getTicks();
-//			List<ChatLine<IReorderingProcessor>> lines = chatGui.drawnChatLines;
-//			int shift = chatGui.scrollPos;
-//
-//			int idx = shift;
-//
-//			while (idx < lines.size() && (idx - shift) < chatGui.getLineCount()) {
-//				ChatLine<IReorderingProcessor> line = lines.get(idx);
-//				String before = "";
-//
-//				IReorderingProcessor lineProperties = line.getLineString();
-//
-//				if (lineProperties instanceof ITextComponent) { // <- wire: not a valid cast
-//					ITextComponent lineComponent = (ITextComponent) lineProperties;
-//
-//					String currentText = TextFormatting.getTextWithoutFormattingCodes(lineComponent.getString());
-//					if (currentText != null && currentText.startsWith("   "))
-//						render(mc, chatGui, updateCounter, before, line, idx - shift, lineComponent);
-//					before += currentText;
-//
-//					for (ITextComponent sibling : lineComponent.getSiblings()) {
-//						currentText = TextFormatting.getTextWithoutFormattingCodes(sibling.getString());
-//						if (currentText != null && currentText.startsWith("   "))
-//							render(mc, chatGui, updateCounter, before, line, idx - shift, sibling);
-//						before += currentText;
-//					}
-//				}
-//
-//				idx++;
-//			}
-//		}
-//	}
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT) // TODO wire fix this thanks
+	public void renderSymbols(RenderGameOverlayEvent.Post event) {
+		if (!renderItemsInChat)
+			return;
 
-//	@OnlyIn(Dist.CLIENT)
-//	private static void render(Minecraft mc, NewChatGui chatGui, int updateCounter, String before, ChatLine line, int lineHeight, ITextComponent component) {
-//		Style style = component.getStyle();
-//		HoverEvent hoverEvent = style.getHoverEvent();
-//		if (hoverEvent != null && hoverEvent.getAction() == HoverEvent.Action.field_230551_b_) {
-//			HoverEvent.ItemHover contents = hoverEvent.func_240662_a_(HoverEvent.Action.field_230551_b_);
-//
-//			ItemStack stack = contents != null ? contents.func_240689_a_() : ItemStack.EMPTY;
-//
-//			if (stack.isEmpty())
-//				stack = new ItemStack(Blocks.BARRIER); // for invalid icon
-//
-//			int timeSinceCreation = updateCounter - line.getUpdatedCounter();
-//			if (chatGui.getChatOpen()) timeSinceCreation = 0;
-//
-//			if (timeSinceCreation < 200) {
-//				double chatOpacity = mc.gameSettings.chatOpacity * 0.9f + 0.1f;
-//				float fadeOut = MathHelper.clamp((1 - timeSinceCreation / 200f) * 10, 0, 1);
-//				double alpha = fadeOut * fadeOut * chatOpacity;
-//
-//				int x = chatX + 3 + mc.fontRenderer.getStringWidth(before);
-//				int y = chatY - mc.fontRenderer.FONT_HEIGHT * lineHeight;
-//
-//				if (alpha > 0) {
-//					RenderHelper.enableStandardItemLighting();
-//					alphaValue = ((int) (alpha * 255) << 24);
-//
-//					renderItemIntoGUI(mc, mc.getItemRenderer(), stack, x, y);
-//
-//					alphaValue = -1;
-//					RenderHelper.disableStandardItemLighting();
-//				}
-//			}
-//		}
-//	}
-//
-//	public static int transformColor(int src) {
-//		if (alphaValue == -1)
-//			return src;
-//		return (src & RGB_MASK) | alphaValue;
-//	}
-//
-//	public static final int RGB_MASK = 0x00FFFFFF;
-//	private static int alphaValue = -1;
-//
-//	@OnlyIn(Dist.CLIENT)
-//	private static void renderItemIntoGUI(Minecraft mc, ItemRenderer render, ItemStack stack, int x, int y) {
-//		renderItemModelIntoGUI(mc, render, stack, x, y, render.getItemModelWithOverrides(stack, null, null));
-//	}
-//
-//	@OnlyIn(Dist.CLIENT)
-//	private static void renderItemModelIntoGUI(Minecraft mc, ItemRenderer render, ItemStack stack, int x, int y, IBakedModel model) {
-//		TextureManager textureManager = mc.getTextureManager();
-//
-//		RenderSystem.pushMatrix();
-//		textureManager.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-//		textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).setBlurMipmapDirect(false, false);
-//		RenderSystem.enableRescaleNormal();
-//		RenderSystem.enableAlphaTest();
-//		RenderSystem.alphaFunc(GL11.GL_GREATER, 0.1F);
-//		RenderSystem.enableBlend();
-//		RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-//		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-//		RenderSystem.translatef(x - 2, y - 2, -2);
-//		RenderSystem.scalef(0.65f, 0.65f, 0.65f);
-//		render.renderItemIntoGUI(stack, 0, 0);
-//		RenderSystem.disableAlphaTest();
-//		RenderSystem.disableRescaleNormal();
-//		RenderSystem.disableLighting();
-//		RenderSystem.popMatrix();
-//		textureManager.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-//		textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-//	}
+		Minecraft mc = Minecraft.getInstance();
+		IngameGui gameGui = mc.ingameGUI;
+		NewChatGui chatGui = gameGui.getChatGUI();
+		if (event.getType() == RenderGameOverlayEvent.ElementType.CHAT) {
+			int updateCounter = gameGui.getTicks();
+			List<ChatLine<ITextComponent>> lines = chatGui.chatLines;
+			int shift = chatGui.scrollPos;
+
+			int idx = shift;
+
+			while (idx < lines.size() && (idx - shift) < chatGui.getLineCount()) {
+				ChatLine<ITextComponent> line = lines.get(idx);
+				StringBuilder before = new StringBuilder();
+
+				ITextComponent lineProperties = line.getLineString();
+
+				int captureIndex = idx;
+				lineProperties.getComponentWithStyle((style, str) -> {
+					if (str != null && str.startsWith("   ")) {
+						render(mc, chatGui, updateCounter, before.toString(), line, captureIndex - shift, style, str);
+					}
+					before.append(str);
+					return Optional.empty();
+				}, lineProperties.getStyle());
+
+				idx++;
+			}
+		}
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	private static void render(Minecraft mc, NewChatGui chatGui, int updateCounter, String before, ChatLine line, int lineHeight, Style style, String str) {
+		HoverEvent hoverEvent = style.getHoverEvent();
+		if (hoverEvent != null && hoverEvent.getAction() == HoverEvent.Action.SHOW_ITEM) {
+			HoverEvent.ItemHover contents = hoverEvent.getParameter(HoverEvent.Action.SHOW_ITEM);
+
+			ItemStack stack = contents != null ? contents.createStack() : ItemStack.EMPTY;
+
+			if (stack.isEmpty())
+				stack = new ItemStack(Blocks.BARRIER); // for invalid icon
+
+			int timeSinceCreation = updateCounter - line.getUpdatedCounter();
+			if (chatGui.getChatOpen()) timeSinceCreation = 0;
+
+			if (timeSinceCreation < 200) {
+				double chatOpacity = mc.gameSettings.chatOpacity * 0.9f + 0.1f;
+				float fadeOut = MathHelper.clamp((1 - timeSinceCreation / 200f) * 10, 0, 1);
+				double alpha = fadeOut * fadeOut * chatOpacity;
+
+				int x = chatX + 3 + mc.fontRenderer.getStringWidth(before);
+				int y = chatY - mc.fontRenderer.FONT_HEIGHT * lineHeight;
+
+				if (alpha > 0) {
+					RenderHelper.enableStandardItemLighting();
+					alphaValue = ((int) (alpha * 255) << 24);
+
+					renderItemIntoGUI(mc, mc.getItemRenderer(), stack, x, y);
+
+					alphaValue = -1;
+					RenderHelper.disableStandardItemLighting();
+				}
+			}
+		}
+	}
+
+	public static int transformColor(int src) {
+		if (alphaValue == -1)
+			return src;
+		return (src & RGB_MASK) | alphaValue;
+	}
+
+	public static final int RGB_MASK = 0x00FFFFFF;
+	private static int alphaValue = -1;
+
+	@OnlyIn(Dist.CLIENT)
+	private static void renderItemIntoGUI(Minecraft mc, ItemRenderer render, ItemStack stack, int x, int y) {
+		renderItemModelIntoGUI(mc, render, stack, x, y, render.getItemModelWithOverrides(stack, null, null));
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	private static void renderItemModelIntoGUI(Minecraft mc, ItemRenderer render, ItemStack stack, int x, int y, IBakedModel model) {
+		TextureManager textureManager = mc.getTextureManager();
+
+		RenderSystem.pushMatrix();
+		textureManager.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).setBlurMipmapDirect(false, false);
+		RenderSystem.enableRescaleNormal();
+		RenderSystem.enableAlphaTest();
+		RenderSystem.alphaFunc(GL11.GL_GREATER, 0.1F);
+		RenderSystem.enableBlend();
+		RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+		RenderSystem.translatef(x - 2, y - 2, -2);
+		RenderSystem.scalef(0.65f, 0.65f, 0.65f);
+		render.renderItemIntoGUI(stack, 0, 0);
+		RenderSystem.disableAlphaTest();
+		RenderSystem.disableRescaleNormal();
+		RenderSystem.disableLighting();
+		RenderSystem.popMatrix();
+		textureManager.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
+	}
 
 }
