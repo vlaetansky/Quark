vlaetansky/Quark

commit 08b26fe055d3d991aeaa1c62f37abfcb8b2cdaef

Another attempt at #2823

src/main/java/vazkii/quark/content/building/module/CompressedBlocksModule.java
@@ -114,9 +114,11 @@ public class CompressedBlocksModule extends QuarkModule {
 
 	@Override
 	public void loadComplete() {
-		for (Block block : compostable)
-			if(block.asItem() != null)
-				ComposterBlock.CHANCES.put(block, 1F);
+		enqueue(() -> {
+			for(Block block : compostable)
+				if(block.asItem() != null)
+					ComposterBlock.CHANCES.put(block, 1F);
+		});
 			
 		FuelHandler.addFuel(stick_block, stickBlockFuelTime);
 		FuelHandler.addFuel(charcoal_block, charcoalBlockFuelTime);

src/main/java/vazkii/quark/base/module/ModuleLoader.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+import com.google.common.base.Preconditions;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -14,6 +16,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.ParallelDispatchEvent;
 import vazkii.quark.base.Quark;
 import vazkii.quark.base.block.IQuarkBlock;
 import vazkii.quark.base.item.IQuarkItem;
@@ -27,6 +30,7 @@ public final class ModuleLoader {
 	
 	private ConfigResolver config;
 	private boolean clientTicked = false;
+	private ParallelDispatchEvent event;
 	
 	private ModuleLoader() { }
 	
@@ -64,15 +68,19 @@ public final class ModuleLoader {
 		dispatch(QuarkModule::configChangedClient);
 	}
 	
-	public void setup() {
+	public void setup(ParallelDispatchEvent event) {
+		this.event = event;
 		dispatch(QuarkModule::earlySetup);
 		Quark.proxy.handleQuarkConfigChange();
 		dispatch(QuarkModule::setup);
+		event = null;
 	}
 
 	@OnlyIn(Dist.CLIENT)
-	public void clientSetup() {
+	public void clientSetup(ParallelDispatchEvent event) {
+		this.event = event;
 		dispatch(QuarkModule::clientSetup);
+		event = null;
 	}
 
 	@OnlyIn(Dist.CLIENT)
@@ -90,8 +98,10 @@ public final class ModuleLoader {
 		dispatch(m -> m.postTextureStitch(event));
 	}
 	
-	public void loadComplete() {
+	public void loadComplete(ParallelDispatchEvent event) {
+		this.event = event;
 		dispatch(QuarkModule::loadComplete);
+		event = null;
 	}
 	
 	@OnlyIn(Dist.CLIENT)
@@ -105,6 +115,11 @@ public final class ModuleLoader {
 	
 	private void dispatch(Consumer<QuarkModule> run) {
 		foundModules.values().forEach(run);
+	}
+	
+	void enqueue(Runnable r) {
+		Preconditions.checkNotNull(event);
+		event.enqueueWork(r);
 	}
 	
 	public boolean isModuleEnabled(Class<? extends QuarkModule> moduleClazz) {

src/main/java/vazkii/quark/base/module/QuarkModule.java
@@ -94,6 +94,10 @@ public class QuarkModule {
 		// NO-OP
 	}
 	
+	protected void enqueue(Runnable r) {
+		ModuleLoader.INSTANCE.enqueue(r);
+	}
+	
 	public final void setEnabled(boolean enabled) {
 		configEnabled = enabled;
 		if(firstLoad) {

src/main/java/vazkii/quark/base/proxy/ClientProxy.java
@@ -62,7 +62,7 @@ public class ClientProxy extends CommonProxy {
 
 	public void clientSetup(FMLClientSetupEvent event) {
 		RenderLayerHandler.init();
-		ModuleLoader.INSTANCE.clientSetup();
+		ModuleLoader.INSTANCE.clientSetup(event);
 	}
 
 	public void modelRegistry(ModelRegistryEvent event) {

src/main/java/vazkii/quark/base/proxy/CommonProxy.java
@@ -55,12 +55,12 @@ public class CommonProxy {
 		BrewingHandler.setup();
 		CapabilityHandler.setup();
 		JigsawRegistryHelper.setup();
-		ModuleLoader.INSTANCE.setup();
+		ModuleLoader.INSTANCE.setup(event);
 		initContributorRewards();
 	}
 	
 	public void loadComplete(FMLLoadCompleteEvent event) {
-		ModuleLoader.INSTANCE.loadComplete();
+		ModuleLoader.INSTANCE.loadComplete(event);
 		WorldGenHandler.loadComplete();
 		FuelHandler.addAllWoods();
 	}

src/main/java/vazkii/quark/content/automation/module/DispensersPlaceBlocksModule.java
@@ -35,22 +35,23 @@ import vazkii.quark.base.module.config.Config;
 public class DispensersPlaceBlocksModule extends QuarkModule {
 
 	@Config public static List<String> blacklist = Lists.newArrayList("minecraft:water", "minecraft:lava", "minecraft:fire");
-	
+
 	@Override
-	public void loadComplete() {
+	public void setup() {
 		if(!enabled)
 			return;
-		
-		Map<Item, IDispenseItemBehavior> registry = DispenserBlock.DISPENSE_BEHAVIOR_REGISTRY;
 
-		for(Block b : ForgeRegistries.BLOCKS) {
-			ResourceLocation res = b.getRegistryName();
-			if(!blacklist.contains(Objects.toString(res))) {
-				Item item = b.asItem();
-				if(item instanceof BlockItem && !registry.containsKey(item))
-					registry.put(item, new BlockBehaviour((BlockItem) item));
+		enqueue(() -> {
+			Map<Item, IDispenseItemBehavior> registry = DispenserBlock.DISPENSE_BEHAVIOR_REGISTRY;
+			for(Block b : ForgeRegistries.BLOCKS) {
+				ResourceLocation res = b.getRegistryName();
+				if(!blacklist.contains(Objects.toString(res))) {
+					Item item = b.asItem();
+					if(item instanceof BlockItem && !registry.containsKey(item))
+						registry.put(item, new BlockBehaviour((BlockItem) item));
+				}
 			}
-		}
+		});
 	}
 
 	public static class BlockBehaviour extends OptionalDispenseBehavior {
@@ -94,12 +95,12 @@ public class DispensersPlaceBlocksModule extends QuarkModule {
 			replaceClicked = worldIn.getBlockState(func_242401_i().getPos()).isReplaceable(this); // func_242401_i = getRayTraceResult
 			direction = p_i50051_3_;
 		}
-		
+
 		@Override
 		public boolean canPlace() {
 			return replaceClicked;
 		}
-		
+
 		@Override
 		public Direction getNearestLookingDirection() {
 			return direction.getOpposite();

src/main/java/vazkii/quark/content/building/module/ThatchModule.java
@@ -25,7 +25,7 @@ public class ThatchModule extends QuarkModule {
 	
 	@Override
 	public void loadComplete() {
-		ComposterBlock.CHANCES.put(thatch.asItem(), 0.65F);
+		enqueue(() -> ComposterBlock.CHANCES.put(thatch.asItem(), 0.65F));
 	}
 	
 }

src/main/java/vazkii/quark/content/world/module/BlossomTreesModule.java
@@ -48,16 +48,15 @@ public class BlossomTreesModule extends QuarkModule {
 	public void setup() {
 		for(BlossomTree tree : trees.keySet())
 			WorldGenHandler.addGenerator(this, new BlossomTreeGenerator(trees.get(tree), tree), Decoration.TOP_LAYER_MODIFICATION, WorldGenWeights.BLOSSOM_TREES);
-	}
-	
-	@Override
-	public void loadComplete() {
-		for(BlossomTree tree : trees.keySet()) {
-			if(tree.leaf.getBlock().asItem() != null)
-				ComposterBlock.CHANCES.put(tree.leaf.getBlock().asItem(), 0.3F);
-			if(tree.sapling.asItem() != null)
-				ComposterBlock.CHANCES.put(tree.sapling.asItem(), 0.3F);
-		}
+
+		enqueue(() -> {
+			for(BlossomTree tree : trees.keySet()) {
+				if(tree.leaf.getBlock().asItem() != null)
+					ComposterBlock.CHANCES.put(tree.leaf.getBlock().asItem(), 0.3F);
+				if(tree.sapling.asItem() != null)
+					ComposterBlock.CHANCES.put(tree.sapling.asItem(), 0.3F);
+			}
+		});
 	}
 	
 	private void add(String colorName, MaterialColor color, BlossomTreeConfig config) {

commit 5d5115c193dfc868f8130e29fe90e9ea75d5b9fd

Fix composter concurrency issue, see #2823. Also fix regression in compressedblocks introduced in 08b26fe055d3d991aeaa1c62f37abfcb8b2cdaef

src/main/java/vazkii/quark/content/building/module/CompressedBlocksModule.java
@@ -117,7 +117,7 @@ public class CompressedBlocksModule extends QuarkModule {
 		enqueue(() -> {
 			for(Block block : compostable)
 				if(block.asItem() != null)
-					ComposterBlock.CHANCES.put(block, 1F);
+					ComposterBlock.CHANCES.put(block.asItem(), 1F);
 		});
 			
 		FuelHandler.addFuel(stick_block, stickBlockFuelTime);

src/main/java/vazkii/quark/content/building/module/LeafCarpetModule.java
@@ -32,9 +32,12 @@ public class LeafCarpetModule extends QuarkModule {
 	
 	@Override
 	public void loadComplete() {
-		for(LeafCarpetBlock c : carpets)
-			if(c.asItem() != null)
-				ComposterBlock.CHANCES.put(c.asItem(), 0.2F);
+		enqueue(() -> {
+			for(LeafCarpetBlock c : carpets) {
+				if(c.asItem() != null)
+					ComposterBlock.CHANCES.put(c.asItem(), 0.2F);
+			}
+		});
 	}
 	
 	private void carpet(Block base) {