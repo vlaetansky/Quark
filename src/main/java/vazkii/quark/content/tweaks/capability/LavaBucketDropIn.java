6299dbcaf20c439ab5e971fc3c337c3607fc1212
Vasco Lavos <vazkii@hotmail.com>
2021-02-02 18:30:43 +0000

    Better checks for lava bucket deleting

--- src/main/java/vazkii/quark/content/tweaks/capability/LavaBucketDropIn.java
+++ src/main/java/vazkii/quark/content/tweaks/capability/LavaBucketDropIn.java
@@ -2,18 +2,25 @@ package vazkii.quark.content.tweaks.capability;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import vazkii.arl.util.AbstractDropIn;
+import vazkii.quark.base.handler.SimilarBlockTypeHandler;
 
 public class LavaBucketDropIn extends AbstractDropIn {
 
 	@Override
 	public boolean canDropItemIn(PlayerEntity player, ItemStack stack, ItemStack incoming) {
-		return true;
+		return !player.isCreative() && !incoming.getItem().isImmuneToFire() && !SimilarBlockTypeHandler.isShulkerBox(incoming);
 	}
 
 	@Override
 	public ItemStack dropItemIn(PlayerEntity player, ItemStack stack, ItemStack incoming) {
 		incoming.setCount(0);
+		
+		if(!player.world.isRemote)
+			player.world.playSound(null, player.getPosition(), SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.PLAYERS, 0.25F, 2F + (float) Math.random());
+		
 		return stack;
 	}
 
