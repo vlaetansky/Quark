55d4d7a5d517de8f159f32172728f99e6bbd2f12
Vasco Lavos <vazkii@hotmail.com>
2021-05-25 20:36:18 +0100

    Fix #3153

--- src/main/java/vazkii/quark/content/client/module/ChestSearchingModule.java
+++ src/main/java/vazkii/quark/content/client/module/ChestSearchingModule.java
@@ -104,9 +104,11 @@ public class ChestSearchingModule extends QuarkModule {
 	}
 
 	private void updateSearchStatus() {
-		searchBar.setEnabled(searchEnabled);
-		searchBar.setVisible(searchEnabled);
-		searchBar.setFocused2(searchEnabled);
+		if(searchBar != null) {
+			searchBar.setEnabled(searchEnabled);
+			searchBar.setVisible(searchEnabled);
+			searchBar.setFocused2(searchEnabled);
+		}
 	}
 
 	@SubscribeEvent
