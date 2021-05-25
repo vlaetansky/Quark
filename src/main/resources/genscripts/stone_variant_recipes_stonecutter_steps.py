02780e956fb44e3edd72119de815820312cb4816
Vasco Lavos <vazkii@hotmail.com>
2021-05-25 20:34:55 +0100

    Fix #2895

---
+++ src/main/resources/genscripts/stone_variant_recipes_stonecutter_steps.py
@@ -0,0 +1,9 @@
+from jsongen import *
+import re
+
+# Use category=... and flag=... to set the flags for this one
+
+copy([
+	('unique/recipe_stone_variant_stonecutting.json', 'data/{modid}/recipes/building/stonecutting/stonevariants/{name}_{suff}.json')
+])
+
