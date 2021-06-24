d6bacfd95f004182ff5d6cad93664b55045fdf60
Vasco Lavos <vazkii@hotmail.com>
2021-06-24 20:05:01 +0100

    no titor

--- src/main/java/vazkii/quark/base/Quark.java
+++ src/main/java/vazkii/quark/base/Quark.java
@@ -1,21 +1,20 @@
 package vazkii.quark.base;
 
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.common.Mod;
-import vazkii.quark.base.proxy.ClientProxy;
-import vazkii.quark.base.proxy.CommonProxy;
-
 import java.lang.management.ManagementFactory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import vazkii.quark.base.proxy.ClientProxy;
+import vazkii.quark.base.proxy.CommonProxy;
 
 @Mod(Quark.MOD_ID)
 public class Quark {
 
 	public static final String MOD_ID = "quark";
 	public static final String ODDITIES_ID = "quarkoddities";
-	public static final String TITOR_ID = "quarktitor";
 
 	public static final boolean DEBUG_MODE = ManagementFactory.getRuntimeMXBean().getInputArguments().toString().indexOf("-agentlib:jdwp") > 0;
 	
--- src/main/java/vazkii/quark/base/module/ModuleCategory.java
+++ src/main/java/vazkii/quark/base/module/ModuleCategory.java
@@ -19,7 +19,6 @@ public enum ModuleCategory {
 	MOBS("mobs", Items.PIG_SPAWN_EGG),
 	CLIENT("client", Items.ENDER_EYE),
 	ODDITIES("oddities", Items.CHORUS_FRUIT, Quark.ODDITIES_ID),
-	TITOR("titor", Items.CLOCK, Quark.TITOR_ID),
 	EXPERIMENTAL("experimental", Items.TNT);
 	
 	public final String name;
