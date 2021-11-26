9ebf1ae4666540042c0beacc10816146a3cabc85
Vasco Lavos <vazkii@hotmail.com>
2021-11-26 19:06:31 +0000

    Fix #3436

--- src/main/java/vazkii/quark/base/block/QuarkGlassBlock.java
+++ src/main/java/vazkii/quark/base/block/QuarkGlassBlock.java
@@ -1,18 +1,20 @@
 package vazkii.quark.base.block;
 
+import javax.annotation.Nonnull;
+
 import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockDisplayReader;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import vazkii.quark.base.module.QuarkModule;
-
-import javax.annotation.Nonnull;
 
 /**
  * @author WireSegal
@@ -20,38 +22,43 @@ import javax.annotation.Nonnull;
  */
 public class QuarkGlassBlock extends QuarkBlock {
 
-    public QuarkGlassBlock(String regname, QuarkModule module, ItemGroup creativeTab, Properties properties) {
-        super(regname, module, creativeTab, properties
-                .notSolid()
-                .setAllowsSpawn((state, world, pos, entityType) -> false)
-                .setOpaque((state, world, pos) -> false)
-                .setSuffocates((state, world, pos) -> false)
-                .setBlocksVision((state, world, pos) -> false));
-    }
+	public QuarkGlassBlock(String regname, QuarkModule module, ItemGroup creativeTab, Properties properties) {
+		super(regname, module, creativeTab, properties
+				.notSolid()
+				.setAllowsSpawn((state, world, pos, entityType) -> false)
+				.setOpaque((state, world, pos) -> false)
+				.setSuffocates((state, world, pos) -> false)
+				.setBlocksVision((state, world, pos) -> false));
+	}
 
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    @SuppressWarnings("deprecation")
-    public boolean isSideInvisible(@Nonnull BlockState state, BlockState adjacentBlockState, @Nonnull Direction side) {
-        return adjacentBlockState.isIn(this) || super.isSideInvisible(state, adjacentBlockState, side);
-    }
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	@SuppressWarnings("deprecation")
+	public boolean isSideInvisible(@Nonnull BlockState state, BlockState adjacentBlockState, @Nonnull Direction side) {
+		return adjacentBlockState.isIn(this) || super.isSideInvisible(state, adjacentBlockState, side);
+	}
 
-    @Override
-    @Nonnull
-    @SuppressWarnings("deprecation")
-    public VoxelShape getRayTraceShape(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
-        return VoxelShapes.empty();
-    }
+	@Override
+	@Nonnull
+	@SuppressWarnings("deprecation")
+	public VoxelShape getRayTraceShape(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
+		return VoxelShapes.empty();
+	}
 
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    public float getAmbientOcclusionLightValue(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos) {
-        return 1.0F;
-    }
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public float getAmbientOcclusionLightValue(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos) {
+		return 1.0F;
+	}
 
-    @Override
-    public boolean propagatesSkylightDown(@Nonnull BlockState state, @Nonnull IBlockReader reader, @Nonnull BlockPos pos) {
-        return true;
-    }
+	@Override
+	public boolean propagatesSkylightDown(@Nonnull BlockState state, @Nonnull IBlockReader reader, @Nonnull BlockPos pos) {
+		return true;
+	}
+
+	@Override
+	public boolean shouldDisplayFluidOverlay(BlockState state, IBlockDisplayReader world, BlockPos pos, FluidState fluidState) {
+		return true;
+	}
 
 }
