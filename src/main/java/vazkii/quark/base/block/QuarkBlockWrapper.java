f507e83f0e70665cd05ca82a7f2a8cbd4f8cac5f
Vasco Lavos <vazkii@hotmail.com>
2021-12-04 08:55:57 +0000

    Calcite files

---
+++ src/main/java/vazkii/quark/base/block/QuarkBlockWrapper.java
@@ -0,0 +1,56 @@
+package vazkii.quark.base.block;
+
+import java.util.Set;
+import java.util.function.BooleanSupplier;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.IPlantable;
+import vazkii.quark.base.module.QuarkModule;
+
+// Wrapper to allow vanilla blocks to be treated as quark blocks contextualized under a module
+public class QuarkBlockWrapper implements IQuarkBlock {
+
+	public final Block parent;
+	public final QuarkModule module;
+	
+	public QuarkBlockWrapper(Block parent, QuarkModule module) {
+		this.parent = parent;
+		this.module = module;
+	}
+	
+	@Override
+	public Block getBlock() {
+		return parent;
+	}
+
+	@Override
+	public QuarkModule getModule() {
+		return module;
+	}
+
+	@Override
+	public IQuarkBlock setCondition(BooleanSupplier condition) {
+		return this;
+	}
+
+	@Override
+	public boolean doesConditionApply() {
+		return false;
+	}
+
+	@Override
+	public Set<ResourceLocation> getTags() {
+		return parent.getTags();
+	}
+
+	@Override
+	public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction facing, IPlantable plantable) {
+		return false;
+	}
+	
+}
