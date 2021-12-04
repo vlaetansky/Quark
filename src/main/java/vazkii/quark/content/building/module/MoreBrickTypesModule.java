842bd40f86de2d265dfd602efbb9082710745470
Vasco Lavos <vazkii@hotmail.com>
2021-12-04 09:08:33 +0000

    Merge Cobblestone Bricks and More Brick Types

--- src/main/java/vazkii/quark/content/building/module/MoreBrickTypesModule.java
+++ src/main/java/vazkii/quark/content/building/module/MoreBrickTypesModule.java
@@ -16,26 +16,43 @@ import vazkii.quark.base.module.config.Config;
 @LoadModule(category = ModuleCategory.BUILDING)
 public class MoreBrickTypesModule extends QuarkModule {
 
-	@Config(flag = "blue_nether_bricks",
-			description = "This also comes with a utility recipe for Red Nether Bricks") 
+	@Config(flag = "blue_nether_bricks", description = "This also comes with a utility recipe for Red Nether Bricks") 
 	public boolean enableBlueNetherBricks = true;
 
-	@Config(flag = "sandstone_bricks",
-			description = "This also includes Red Sandstone Bricks and Soul Sandstone Bricks") 
+	@Config(flag = "sandstone_bricks", description = "This also includes Red Sandstone Bricks and Soul Sandstone Bricks") 
 	public boolean enableSandstoneBricks = true;
+
+	@Config(flag = "cobblestone_bricks", description = "This also includes Mossy Cobblestone Bricks")
+	private static boolean enableCobblestoneBricks = true;
+	
+	@Config(flag = "blackstone_bricks", description = "Requires Cobblestone Bricks to be enabled")
+	private static boolean enableBlackstoneBricks = true;
+	
+	@Config(flag = "dirt_bricks", description = "Requires Cobblestone Bricks to be enabled")
+	private static boolean enableDirtBricks = true;
+	
+	@Config(flag = "netherrack_bricks", description = "Requires Cobblestone Bricks to be enabled")
+	private static boolean enableNetherrackBricks = true;
 	
 	@Override
 	public void construct() {
 		add("blue_nether", Blocks.NETHER_BRICKS, () -> enableBlueNetherBricks);
+		
 		add("sandstone", Blocks.SANDSTONE, () -> enableSandstoneBricks);
 		add("red_sandstone", Blocks.RED_SANDSTONE, () -> enableSandstoneBricks);
 		add("soul_sandstone", Blocks.SANDSTONE, () -> enableSandstoneBricks && ModuleLoader.INSTANCE.isModuleEnabled(SoulSandstoneModule.class));
+		
+		add("cobblestone", Blocks.COBBLESTONE, () -> enableCobblestoneBricks);
+		add("mossy_cobblestone", Blocks.MOSSY_COBBLESTONE, () -> enableCobblestoneBricks);
+		
+		add("blackstone", Blocks.BLACKSTONE, () -> enableBlackstoneBricks && enableCobblestoneBricks);
+		add("dirt", Blocks.DIRT, () -> enableDirtBricks && enableCobblestoneBricks);
+		add("netherrack", Blocks.NETHERRACK, () -> enableNetherrackBricks && enableCobblestoneBricks);
 	}
 	
 	private void add(String name, Block parent, BooleanSupplier cond) {
 		VariantHandler.addSlabStairsWall(new QuarkBlock(name + "_bricks", this, CreativeModeTab.TAB_BUILDING_BLOCKS, 
 				Block.Properties.copy(parent)
-				.strength(2F, 6F)
 				.requiresCorrectToolForDrops())
 //				.harvestTool(ToolType.PICKAXE)) TODO TAG
 				.setCondition(cond));
