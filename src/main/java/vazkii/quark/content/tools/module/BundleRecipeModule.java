07c940f1dd0ef7024eaa705b9072879402025e5a
Vasco Lavos <vazkii@hotmail.com>
2021-12-08 12:47:14 +0000

    Craftable bundles

---
+++ src/main/java/vazkii/quark/content/tools/module/BundleRecipeModule.java
@@ -0,0 +1,12 @@
+package vazkii.quark.content.tools.module;
+
+import vazkii.quark.base.module.LoadModule;
+import vazkii.quark.base.module.ModuleCategory;
+import vazkii.quark.base.module.QuarkModule;
+
+@LoadModule(category = ModuleCategory.TOOLS)
+public class BundleRecipeModule extends QuarkModule {
+
+	// this just serves to toggle the flag
+	
+}
