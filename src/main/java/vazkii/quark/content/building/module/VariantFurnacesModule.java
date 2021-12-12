d383b025a052616d9b514acbc9b18dbad7ec0a7c
Vasco Lavos <vazkii@hotmail.com>
2021-12-12 16:45:30 +0000

    Fix furnace sounds and lighting

--- src/main/java/vazkii/quark/content/building/module/VariantFurnacesModule.java
+++ src/main/java/vazkii/quark/content/building/module/VariantFurnacesModule.java
@@ -1,9 +1,13 @@
 package vazkii.quark.content.building.module;
 
+import java.util.function.ToIntFunction;
+
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import vazkii.arl.util.RegistryHelper;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.ModuleCategory;
@@ -16,14 +20,18 @@ import vazkii.quark.content.building.block.be.VariantFurnaceBlockEntity;
 public class VariantFurnacesModule extends QuarkModule {
 
 	public static BlockEntityType<VariantFurnaceBlockEntity> blockEntityType;
-	
+
 	@Override
 	public void construct() {
-		Block deepslateFurnace = new VariantFurnaceBlock("deepslate", this, Properties.copy(Blocks.DEEPSLATE));
-		Block blackstoneFurnace = new SoulFurnaceBlock("blackstone", this, Properties.copy(Blocks.DEEPSLATE));
-		
-        blockEntityType = BlockEntityType.Builder.of(VariantFurnaceBlockEntity::new, deepslateFurnace, blackstoneFurnace).build(null);
-        RegistryHelper.register(blockEntityType, "variant_furnace");
+		Block deepslateFurnace = new VariantFurnaceBlock("deepslate", this, Properties.copy(Blocks.DEEPSLATE).lightLevel(litBlockEmission(13)));
+		Block blackstoneFurnace = new SoulFurnaceBlock("blackstone", this, Properties.copy(Blocks.BLACKSTONE).lightLevel(litBlockEmission(13)));
+
+		blockEntityType = BlockEntityType.Builder.of(VariantFurnaceBlockEntity::new, deepslateFurnace, blackstoneFurnace).build(null);
+		RegistryHelper.register(blockEntityType, "variant_furnace");
 	}
-	
+
+	private static ToIntFunction<BlockState> litBlockEmission(int lvl) {
+		return s -> s.getValue(BlockStateProperties.LIT) ? lvl : 0;
+	}
+
 }
