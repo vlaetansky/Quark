4e47d6c04a4cfb0aebb94f1677de3bd281425005
Vasco Lavos <vazkii@hotmail.com>
2021-12-16 19:26:51 +0000

    Prepare for mass adding recipes for new vert slabs

---
+++ src/main/resources/genscripts/vanilla_stonecutter_vertical_slab.py
@@ -0,0 +1,8 @@
+from jsongen import *
+import re
+
+# Use category=... and flag=... to set the flags for this one
+
+copy([	
+	('recipe_vertical_slab_stonecutter_vanilla.json', 'data/{modid}/recipes/building/stonecutter/vertslabs/{name}_vertical_slab.json')
+])
