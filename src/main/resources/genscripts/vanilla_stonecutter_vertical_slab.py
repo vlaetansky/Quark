65a6824c5bd5c20ba147eec4d76eeae495e64daf
Vasco Lavos <vazkii@hotmail.com>
2021-12-16 19:27:54 +0000

    Catch variants

--- src/main/resources/genscripts/vanilla_stonecutter_vertical_slab.py
+++ src/main/resources/genscripts/vanilla_stonecutter_vertical_slab.py
@@ -4,5 +4,5 @@ import re
 # Use category=... and flag=... to set the flags for this one
 
 copy([	
-	('recipe_vertical_slab_stonecutter_vanilla.json', 'data/{modid}/recipes/building/stonecutter/vertslabs/{name}_vertical_slab.json')
+	('recipe_vertical_slab_stonecutter_vanilla.json', 'data/{modid}/recipes/building/stonecutter/vertslabs/{name}_vertical_slab_from_{from}.json')
 ])
