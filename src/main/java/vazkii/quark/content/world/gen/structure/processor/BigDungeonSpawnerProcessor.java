f407cf25442f8de697f8778514b892825e5d8599
Vasco Lavos <vazkii@hotmail.com>
2021-12-18 18:28:35 +0000

    Fix big dungeon spawners changing to zombie

--- src/main/java/vazkii/quark/content/world/gen/structure/processor/BigDungeonSpawnerProcessor.java
+++ src/main/java/vazkii/quark/content/world/gen/structure/processor/BigDungeonSpawnerProcessor.java
@@ -1,12 +1,15 @@
 package vazkii.quark.content.world.gen.structure.processor;
 
+import java.util.Optional;
 import java.util.Random;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BaseSpawner;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.SpawnData;
 import net.minecraft.world.level.block.SpawnerBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
@@ -33,12 +36,17 @@ public class BigDungeonSpawnerProcessor extends StructureProcessor {
     			SpawnerBlockEntity spawner = (SpawnerBlockEntity) tile;
     			BaseSpawner logic = spawner.getSpawner();
     			
+    			EntityType<?> typeToSet = null;
     			double val = rand.nextDouble();
     			if(val > 0.95)
-    				logic.setEntityId(EntityType.CREEPER);
+    				typeToSet = EntityType.CREEPER;
     			else if(val > 0.5)
-    				logic.setEntityId(EntityType.SKELETON);
-    			else logic.setEntityId(EntityType.ZOMBIE);
+    				typeToSet = EntityType.SKELETON;
+    			else typeToSet = EntityType.ZOMBIE;	
+    			
+    			CompoundTag tag = new CompoundTag();
+    			tag.putString("id", typeToSet.getRegistryName().toString());
+    			logic.spawnPotentials = SimpleWeightedRandomList.single(new SpawnData(tag, Optional.empty()));
     			
     			CompoundTag nbt = spawner.saveWithFullMetadata();
     			return new StructureBlockInfo(blockInfo.pos, blockInfo.state, nbt);
