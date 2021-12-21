b3304ada05ba25df0b516e19ddc4b2fd693f192e
Vasco Lavos <vazkii@hotmail.com>
2021-12-21 16:36:55 +0000

    Corundum clusters use amethyst hitbox

--- src/main/java/vazkii/quark/content/world/block/CorundumClusterBlock.java
+++ src/main/java/vazkii/quark/content/world/block/CorundumClusterBlock.java
@@ -29,30 +29,68 @@ import vazkii.quark.base.handler.RenderLayerHandler;
 import vazkii.quark.base.handler.RenderLayerHandler.RenderTypeSkeleton;
 
 public class CorundumClusterBlock extends QuarkBlock implements SimpleWaterloggedBlock {
-	
+
 	public final CorundumBlock base;
-	
+
 	public static final EnumProperty<Direction> FACING = BlockStateProperties.FACING;
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+	protected static VoxelShape northAabb;
+	protected static VoxelShape southAabb;
+	protected static VoxelShape eastAabb;
+	protected static VoxelShape westAabb;
+	protected static VoxelShape upAabb;
+	protected static VoxelShape downAabb;
+
+	static {
+		int p_152015_ = 7;
+		int p_152016_ = 3;
+		upAabb = Block.box((double)p_152016_, 0.0D, (double)p_152016_, (double)(16 - p_152016_), (double)p_152015_, (double)(16 - p_152016_));
+		downAabb = Block.box((double)p_152016_, (double)(16 - p_152015_), (double)p_152016_, (double)(16 - p_152016_), 16.0D, (double)(16 - p_152016_));
+		northAabb = Block.box((double)p_152016_, (double)p_152016_, (double)(16 - p_152015_), (double)(16 - p_152016_), (double)(16 - p_152016_), 16.0D);
+		southAabb = Block.box((double)p_152016_, (double)p_152016_, 0.0D, (double)(16 - p_152016_), (double)(16 - p_152016_), (double)p_152015_);
+		eastAabb = Block.box(0.0D, (double)p_152016_, (double)p_152016_, (double)p_152015_, (double)(16 - p_152016_), (double)(16 - p_152016_));
+		westAabb = Block.box((double)(16 - p_152015_), (double)p_152016_, (double)p_152016_, 16.0D, (double)(16 - p_152016_), (double)(16 - p_152016_));
+	}
 
 	public CorundumClusterBlock(CorundumBlock base) {
 		super(base.getRegistryName().getPath() + "_cluster", base.getModule(), CreativeModeTab.TAB_DECORATIONS, 
 				Block.Properties.copy(base)
 				.sound(SoundType.AMETHYST_CLUSTER));
-		
+
 		this.base = base;
 		base.cluster = this;
-		
+
 		RenderLayerHandler.setRenderType(this, RenderTypeSkeleton.CUTOUT);
 		registerDefaultState(defaultBlockState().setValue(FACING, Direction.DOWN).setValue(WATERLOGGED, false));
 	}
-	
+
+	@Override
+	public VoxelShape getShape(BlockState p_152021_, BlockGetter p_152022_, BlockPos p_152023_, CollisionContext p_152024_) {
+		Direction direction = p_152021_.getValue(FACING);
+		switch(direction) {
+		case NORTH:
+			return northAabb;
+		case SOUTH:
+			return southAabb;
+		case EAST:
+			return eastAabb;
+		case WEST:
+			return westAabb;
+		case DOWN:
+			return downAabb;
+		case UP:
+		default:
+			return upAabb;
+		}
+	}
+
 	@Override
 	public void neighborChanged(BlockState state, Level worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
 		if(!canSurvive(state, worldIn, pos))
 			worldIn.destroyBlock(pos, true);
 	}
-	
+
 	@Override
 	public boolean canSurvive(BlockState state, LevelReader worldIn, BlockPos pos) {
 		Direction dir = state.getValue(FACING);
@@ -61,12 +99,12 @@ public class CorundumClusterBlock extends QuarkBlock implements SimpleWaterlogge
 		return offState.isFaceSturdy(worldIn, off, dir);
 	}
 
-	
+
 	@Override
 	public VoxelShape getBlockSupportShape(BlockState state, BlockGetter reader, BlockPos pos) {
 		return Shapes.empty();
 	}
-	
+
 	@Override
 	public VoxelShape getCollisionShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
 		return Shapes.empty();
@@ -76,13 +114,13 @@ public class CorundumClusterBlock extends QuarkBlock implements SimpleWaterlogge
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
 		return defaultBlockState().setValue(FACING, context.getClickedFace()).setValue(WATERLOGGED, context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER);
 	}
-	
+
 	@Nonnull
 	@Override
 	public FluidState getFluidState(BlockState state) {
 		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
 	}
-	
+
 	@Override
 	public boolean isPathfindable(@Nonnull BlockState state, @Nonnull BlockGetter worldIn, @Nonnull BlockPos pos, PathComputationType type) {
 		return type == PathComputationType.WATER && worldIn.getFluidState(pos).is(FluidTags.WATER); 
@@ -92,5 +130,5 @@ public class CorundumClusterBlock extends QuarkBlock implements SimpleWaterlogge
 	protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
 		builder.add(FACING, WATERLOGGED);
 	}
-	
+
 }
