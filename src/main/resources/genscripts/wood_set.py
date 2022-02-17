53999f30f1fa0bd64b6e1b222c05952ff056301e
Vasco Lavos <vazkii@hotmail.com>
2022-02-17 13:02:36 +0000

    Move textures

--- src/main/resources/genscripts/wood_set.py
+++ src/main/resources/genscripts/wood_set.py
@@ -1,6 +1,8 @@
 import sys
 from os import system as run
 
+category = 'world'
+
 def main():
 	for arg in sys.argv:
 		if not '.py' in arg:
@@ -10,5 +12,6 @@ def makeWood(type):
 	run(f"py generic_block.py {type}_planks")
 	run(f"py pillar.py {type}_log stripped_{type}_log")
 	run(f"py wood_block.py {type} stripped_{type}")
+	run(f"py stairs_slabs.py category={category} {type}_planks")
 
 main()
