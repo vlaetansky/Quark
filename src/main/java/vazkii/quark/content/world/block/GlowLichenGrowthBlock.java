0743bd11daf512258eab843c68f0a9fb172d6a2a
Vasco Lavos <vazkii@hotmail.com>
2022-02-18 12:46:34 +0000

    Glow lichen growth bone mealable

--- src/main/java/vazkii/quark/content/world/block/GlowLichenGrowthBlock.java
+++ src/main/java/vazkii/quark/content/world/block/GlowLichenGrowthBlock.java
@@ -1,24 +1,31 @@
 package vazkii.quark.content.world.block;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+
+import com.google.common.collect.Lists;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import vazkii.quark.base.block.QuarkBushBlock;
+import vazkii.quark.base.handler.MiscUtil;
 import vazkii.quark.base.module.QuarkModule;
 
-public class GlowLichenGrowthBlock extends QuarkBushBlock {
+public class GlowLichenGrowthBlock extends QuarkBushBlock implements BonemealableBlock {
 
 	protected static final VoxelShape SHAPE = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 6.0D, 11.0D);
 
@@ -57,7 +64,39 @@ public class GlowLichenGrowthBlock extends QuarkBushBlock {
 
 	@Override
 	protected boolean mayPlaceOn(BlockState p_54894_, BlockGetter p_54895_, BlockPos p_54896_) {
-		return p_54894_.isFaceSturdy(p_54895_, p_54896_.below(), Direction.UP);
+		return p_54894_.isFaceSturdy(p_54895_, p_54896_, Direction.UP);
+	}
+
+	@Override
+	public boolean isValidBonemealTarget(BlockGetter world, BlockPos pos, BlockState state, boolean idkmansomething) {
+		for(Direction dir : MiscUtil.HORIZONTALS)
+			if(canSpread(world, pos.relative(dir)))
+				return true;
+
+		return false;
+	}
+
+	@Override
+	public boolean isBonemealSuccess(Level p_50901_, Random p_50902_, BlockPos p_50903_, BlockState p_50904_) {
+		return true;
+	}
+
+	@Override
+	public void performBonemeal(ServerLevel world, Random rand, BlockPos pos, BlockState state) {
+		List<Direction> list = Lists.newArrayList(MiscUtil.HORIZONTALS);
+		Collections.shuffle(list, rand);
+		for(Direction dir : list) {
+			BlockPos offPos = pos.relative(dir);
+			if(canSpread(world, offPos)) {
+				world.setBlock(offPos, state, 3);
+				return;
+			}
+		}
+	}
+
+	private boolean canSpread(BlockGetter world, BlockPos pos) {
+		BlockPos below = pos.below();
+		return world.getBlockState(pos).isAir() && mayPlaceOn(world.getBlockState(below), world, below);
 	}
 
 }
