9faf7e7db3b9b6bd3a282fb413d0092086445d20
yrsegal@gmail.com <yrsegal@gmail.com>
2022-04-01 13:59:22 -0400

    first-class nepotism

--- src/main/java/vazkii/quark/content/tweaks/module/PatTheDogsModule.java
+++ src/main/java/vazkii/quark/content/tweaks/module/PatTheDogsModule.java
@@ -69,73 +69,73 @@ public class PatTheDogsModule extends QuarkModule {
 
 					WantLoveGoal.setPetTime(wolf);
 
-					if (wolf instanceof Foxhound && !player.isInWater() && !player.hasEffect(
-						MobEffects.FIRE_RESISTANCE) && !player.getAbilities().invulnerable)
+					if (wolf instanceof Foxhound && !player.isInWater() && !player.hasEffect(MobEffects.FIRE_RESISTANCE)
+							&& !player.getAbilities().invulnerable)
 						player.setSecondsOnFire(5);
 				}
 
 				event.setCanceled(true);
-			} else if (petAllMobs && event.getTarget() instanceof LivingEntity living && !pettableDenylist.contains(living.getEncodeId())) {
+			} else if ((petAllMobs || event.getTarget() instanceof Player) && event.getTarget() instanceof LivingEntity living && !pettableDenylist.contains(living.getEncodeId())) {
 				if (event.getHand() == InteractionHand.MAIN_HAND) {
-					if (player.level instanceof ServerLevel serverLevel) {
-						var pos = living.getEyePosition();
-						serverLevel.sendParticles(ParticleTypes.HEART, pos.x, pos.y + 0.5, pos.z, 1, 0, 0, 0, 0.1);
-
-						SoundEvent sound = null;
-						float pitchCenter = 1f;
-						if (living instanceof Axolotl) {
-							sound = SoundEvents.AXOLOTL_SPLASH;
-						} else if (living instanceof Cat || living instanceof Ocelot) {
-							sound = SoundEvents.CAT_PURREOW;
-						} else if (living instanceof Chicken) {
-							sound = SoundEvents.CHICKEN_AMBIENT;
-						} else if (living instanceof Cow) {
-							sound = SoundEvents.COW_AMBIENT;
-							pitchCenter = 1.2f;
-						} else if (living instanceof AbstractHorse) {
-							sound = SoundEvents.HORSE_AMBIENT;
-						} else if (living instanceof AbstractFish) {
-							sound = SoundEvents.FISH_SWIM;
-						} else if (living instanceof Fox) {
-							sound = SoundEvents.FOX_SLEEP;
-						} else if (living instanceof Squid) {
-							sound = (living instanceof GlowSquid) ?
+					SoundEvent sound = null;
+					float pitchCenter = 1f;
+					if (living instanceof Axolotl) {
+						sound = SoundEvents.AXOLOTL_SPLASH;
+					} else if (living instanceof Cat || living instanceof Ocelot) {
+						sound = SoundEvents.CAT_PURREOW;
+					} else if (living instanceof Chicken) {
+						sound = SoundEvents.CHICKEN_AMBIENT;
+					} else if (living instanceof Cow) {
+						sound = SoundEvents.COW_AMBIENT;
+						pitchCenter = 1.2f;
+					} else if (living instanceof AbstractHorse) {
+						sound = SoundEvents.HORSE_AMBIENT;
+					} else if (living instanceof AbstractFish) {
+						sound = SoundEvents.FISH_SWIM;
+					} else if (living instanceof Fox) {
+						sound = SoundEvents.FOX_SLEEP;
+					} else if (living instanceof Squid) {
+						sound = (living instanceof GlowSquid) ?
 								SoundEvents.GLOW_SQUID_SQUIRT : SoundEvents.SQUID_SQUIRT;
-							pitchCenter = 1.2f;
-						} else if (living instanceof Parrot) {
-							sound = SoundEvents.PARROT_AMBIENT;
-						} else if (living instanceof Pig) {
-							sound = SoundEvents.PIG_AMBIENT;
-						} else if (living instanceof Rabbit) {
-							sound = SoundEvents.RABBIT_AMBIENT;
-						} else if (living instanceof Sheep) {
-							sound = SoundEvents.SHEEP_AMBIENT;
-						} else if (living instanceof Strider) {
-							sound = SoundEvents.STRIDER_HAPPY;
-						} else if (living instanceof Turtle) {
-							sound = SoundEvents.TURTLE_AMBIENT_LAND;
-						} else if (living instanceof Player pettee) {
-							var uuid = pettee.getStringUUID();
-							sound = switch (uuid) {
-								case "a2ce9382-2518-4752-87b2-c6a5c97f173e" -> // petra_the_kat
-										QuarkSounds.PET_DEVICE;
-								case "29a10dc6-a201-4993-80d8-c847212bc92b", // MacyMacerator
-										"d30d8e38-6f93-4d96-968d-dd6ec5596941" -> // Falkory220
-										QuarkSounds.PET_NEKO;
-								case "d475af59-d73c-42be-90ed-f1a78f10d452" -> // DaniCherryJam
-										QuarkSounds.PET_SLIME;
-								case "458391f5-6303-4649-b416-e4c0d18f837a" -> // yrsegal
-										QuarkSounds.PET_WIRE;
-								default -> null;
-							};
-						}
-						if (sound != null) {
-							living.playSound(sound, 1F, pitchCenter + (float) (Math.random() - 0.5) * 0.5F);
-						}
-					} else player.swing(InteractionHand.MAIN_HAND);
-				}
+						pitchCenter = 1.2f;
+					} else if (living instanceof Parrot) {
+						sound = SoundEvents.PARROT_AMBIENT;
+					} else if (living instanceof Pig) {
+						sound = SoundEvents.PIG_AMBIENT;
+					} else if (living instanceof Rabbit) {
+						sound = SoundEvents.RABBIT_AMBIENT;
+					} else if (living instanceof Sheep) {
+						sound = SoundEvents.SHEEP_AMBIENT;
+					} else if (living instanceof Strider) {
+						sound = SoundEvents.STRIDER_HAPPY;
+					} else if (living instanceof Turtle) {
+						sound = SoundEvents.TURTLE_AMBIENT_LAND;
+					} else if (living instanceof Player pettee) {
+						var uuid = pettee.getStringUUID();
+						sound = switch (uuid) {
+							case "a2ce9382-2518-4752-87b2-c6a5c97f173e" -> // petra_the_kat
+									QuarkSounds.PET_DEVICE;
+							case "29a10dc6-a201-4993-80d8-c847212bc92b", // MacyMacerator
+									"d30d8e38-6f93-4d96-968d-dd6ec5596941" -> // Falkory220
+									QuarkSounds.PET_NEKO;
+							case "d475af59-d73c-42be-90ed-f1a78f10d452" -> // DaniCherryJam
+									QuarkSounds.PET_SLIME;
+							case "458391f5-6303-4649-b416-e4c0d18f837a" -> // yrsegal
+									QuarkSounds.PET_WIRE;
+							default -> null;
+						};
+					}
+					if (sound != null) {
+						if (player.level instanceof ServerLevel serverLevel) {
+							var pos = living.getEyePosition();
+							serverLevel.sendParticles(ParticleTypes.HEART, pos.x, pos.y + 0.5, pos.z, 1, 0, 0, 0, 0.1);
 
-				event.setCanceled(true);
+							living.playSound(sound, 1F, pitchCenter + (float) (Math.random() - 0.5) * 0.5F);
+						} else player.swing(InteractionHand.MAIN_HAND);
+
+						event.setCanceled(true);
+					}
+				}
 			}
 		}
 	}
