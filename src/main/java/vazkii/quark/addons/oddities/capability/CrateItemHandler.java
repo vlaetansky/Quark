a826acf0a027eff17c823a7661e1f38189d3f117
yrsegal@gmail.com <yrsegal@gmail.com>
2022-04-08 10:28:38 -0400

    crates are better now
    fix #3678

---
+++ src/main/java/vazkii/quark/addons/oddities/capability/CrateItemHandler.java
@@ -0,0 +1,121 @@
+package vazkii.quark.addons.oddities.capability;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.util.Mth;
+import net.minecraft.world.Containers;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.items.ItemStackHandler;
+import vazkii.quark.addons.oddities.module.CrateModule;
+import vazkii.quark.base.handler.SortingHandler;
+
+/**
+ * @author WireSegal
+ * Created at 8:53 AM on 4/8/22.
+ */
+public class CrateItemHandler extends ItemStackHandler {
+
+	private boolean needsUpdate = false;
+
+	private final int maxItems;
+
+	public int displayTotal = 0;
+	public int displaySlots = 0;
+
+	public CrateItemHandler() {
+		super(CrateModule.maxItems);
+		maxItems = CrateModule.maxItems;
+	}
+
+	private int getTotal() {
+		int items = 0;
+		for (ItemStack stack : stacks) items += stack.getCount();
+		return items;
+	}
+
+	public void recalculate() {
+		if (!needsUpdate)
+			return;
+		needsUpdate = false;
+
+		displayTotal = 0;
+		displaySlots = 0;
+
+		NonNullList<ItemStack> newStacks = NonNullList.withSize(maxItems, ItemStack.EMPTY);
+		int idx = 0;
+		for (ItemStack stack : stacks) {
+			if (!stack.isEmpty()) {
+				newStacks.set(idx, stack);
+				displayTotal += stack.getCount();
+				displaySlots++;
+				idx++;
+			}
+		}
+		stacks = newStacks;
+	}
+
+	public void clear() {
+		needsUpdate = false;
+		stacks = NonNullList.withSize(maxItems, ItemStack.EMPTY);
+		displayTotal = 0;
+		displaySlots = 0;
+	}
+
+	public boolean isEmpty() {
+		for (ItemStack stack : stacks)
+			if (!stack.isEmpty())
+				return false;
+		return true;
+	}
+
+	public void spill(Level level, BlockPos worldPosition) {
+		SortingHandler.mergeStacks(stacks);
+
+		for(ItemStack stack : stacks)
+			if(!stack.isEmpty())
+				Containers.dropItemStack(level, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), stack);
+	}
+
+	@Override
+	public int getSlotLimit(int slot) {
+		int total = getTotal();
+		return Mth.clamp(maxItems - total, 0, 64);
+	}
+
+	@Override
+	public void onContentsChanged(int slot) {
+		needsUpdate = true;
+	}
+
+	@Override
+	protected void onLoad() {
+		needsUpdate = true;
+		recalculate();
+	}
+
+	@Override
+	public CompoundTag serializeNBT() {
+		ListTag items = new ListTag();
+		for (ItemStack stack : stacks) {
+			if (!stack.isEmpty())
+				items.add(stack.save(new CompoundTag()));
+		}
+		CompoundTag nbt = new CompoundTag();
+		nbt.put("Items", items);
+		return nbt;
+	}
+
+	@Override
+	public void deserializeNBT(CompoundTag nbt) {
+		stacks = NonNullList.withSize(maxItems, ItemStack.EMPTY);
+
+		ListTag items = nbt.getList("Items", Tag.TAG_COMPOUND);
+		for (int i = 0; i < items.size(); i++)
+			stacks.set(i, ItemStack.of(items.getCompound(i)));
+		onLoad();
+	}
+}
--- src/main/java/vazkii/quark/mixin/WeatheringCopperMixin.java
+++ src/main/java/vazkii/quark/mixin/WeatheringCopperMixin.java
@@ -3,9 +3,7 @@ package vazkii.quark.mixin;
 import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 import vazkii.quark.base.block.CustomWeatheringCopper;
 
 import java.util.Optional;
@@ -13,16 +11,28 @@ import java.util.Optional;
 @Mixin(WeatheringCopper.class)
 public interface WeatheringCopperMixin {
 
-	@Inject(method = "getPrevious(Lnet/minecraft/world/level/block/state/BlockState;)Ljava/util/Optional;", at = @At("HEAD"), cancellable = true)
-	private static void getPrevious(BlockState state, CallbackInfoReturnable<Optional<BlockState>> cir) {
+	// Why. Why can I not inject into static interface methods. Why.
+
+	/**
+	 * @author WireSegal
+	 * @reason The list of copper states is missing a way to add other blocks.
+	 */
+	@Overwrite
+	static Optional<BlockState> getPrevious(BlockState state) {
 		if (state.getBlock() instanceof CustomWeatheringCopper copper)
-			cir.setReturnValue(copper.getPrevious(state));
+			return copper.getPrevious(state);
+		return WeatheringCopper.getPrevious(state.getBlock()).map((block) -> block.withPropertiesOf(state));
 	}
 
-	@Inject(method = "getFirst(Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/level/block/state/BlockState;", at = @At("HEAD"), cancellable = true)
-	private static void getFirst(BlockState state, CallbackInfoReturnable<BlockState> cir) {
+	/**
+	 * @author WireSegal
+	 * @reason The list of copper states is missing a way to add other blocks.
+	 */
+	@Overwrite
+	static BlockState getFirst(BlockState state) {
 		if (state.getBlock() instanceof CustomWeatheringCopper copper)
-			cir.setReturnValue(copper.getFirst(state));
+			return copper.getFirst(state);
+		return WeatheringCopper.getFirst(state.getBlock()).withPropertiesOf(state);
 	}
 
 }
