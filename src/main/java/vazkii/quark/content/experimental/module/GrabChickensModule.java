6ec0e8a601f7e6464bb160eb4c140a4d099c08df
Vasco Lavos <vazkii@hotmail.com>
2022-04-08 19:36:58 +0100

    Additional tweaks to chickens

--- src/main/java/vazkii/quark/content/experimental/module/GrabChickensModule.java
+++ src/main/java/vazkii/quark/content/experimental/module/GrabChickensModule.java
@@ -2,6 +2,7 @@ package vazkii.quark.content.experimental.module;
 
 import java.util.List;
 
+import net.minecraft.client.model.ChickenModel;
 import net.minecraft.network.protocol.game.ClientboundSetPassengersPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
@@ -9,6 +10,8 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
@@ -17,9 +20,16 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.ModuleCategory;
 import vazkii.quark.base.module.QuarkModule;
+import vazkii.quark.base.module.config.Config;
 
 @LoadModule(category = ModuleCategory.EXPERIMENTAL, enabledByDefault = false, hasSubscriptions = true)
 public class GrabChickensModule extends QuarkModule {
+	
+	@Config
+	private static boolean needsNoHelmet = true;
+	
+	@Config(description = "Set to 0 to disable")
+	private static int slownessLevel = 1;
 
 	private static boolean staticEnabled;
 	
@@ -34,7 +44,7 @@ public class GrabChickensModule extends QuarkModule {
 		Player player = event.getPlayer();
 		Level level = event.getWorld();
 		
-		if(staticEnabled && event.getHand() == InteractionHand.MAIN_HAND && !player.isCrouching() && target.getType() == EntityType.CHICKEN) {
+		if(staticEnabled && event.getHand() == InteractionHand.MAIN_HAND && !player.isCrouching() && canPlayerHostChicken(player) && target.getType() == EntityType.CHICKEN) {
 			List<Entity> passengers = player.getPassengers();
 			
 			boolean changed = false;
@@ -62,11 +72,34 @@ public class GrabChickensModule extends QuarkModule {
 	@SubscribeEvent
 	public void playerTick(PlayerTickEvent event) {
 		Player player = event.player;
+		Level level = player.level;
+		
 		if(player.hasPassenger(e -> e.getType() == EntityType.CHICKEN)) {
-			MobEffectInstance effect = player.getEffect(MobEffects.SLOW_FALLING);
-			if(effect == null)
+			if(!canPlayerHostChicken(player)) {
+				player.ejectPassengers();
+				
+				if(level instanceof ServerLevel slevel)
+					slevel.getChunkSource().chunkMap.broadcast(player, new ClientboundSetPassengersPacket(player));
+			} else if(!player.hasEffect(MobEffects.SLOW_FALLING)) {
 				player.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 5, 0, true, false));
+				
+				if(slownessLevel > 0 && !player.hasEffect(MobEffects.MOVEMENT_SLOWDOWN))
+					player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 5, slownessLevel - 1, true, false));
+			}
 		}
+	}
+	
+	private boolean canPlayerHostChicken(Player player) {
+		return !needsNoHelmet || player.getItemBySlot(EquipmentSlot.HEAD).isEmpty();
+	}
+	
+	public static void setRenderChickenFeetStatus(Chicken entity, ChickenModel<Chicken> model) {
+		if(!staticEnabled)
+			return;
+		
+		boolean should = entity.getVehicle() == null || entity.getVehicle().getType() != EntityType.PLAYER;
+		model.leftLeg.visible = should;
+		model.rightLeg.visible = should;
 	}
 	
 }
--- src/main/java/vazkii/quark/mixin/client/variants/ChickenRendererMixin.java
+++ src/main/java/vazkii/quark/mixin/client/variants/ChickenRendererMixin.java
@@ -1,18 +1,26 @@
 package vazkii.quark.mixin.client.variants;
 
-import net.minecraft.client.renderer.entity.ChickenRenderer;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.animal.Chicken;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.model.ChickenModel;
+import net.minecraft.client.renderer.entity.ChickenRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.animal.Chicken;
 import vazkii.quark.content.client.module.VariantAnimalTexturesModule;
+import vazkii.quark.content.experimental.module.GrabChickensModule;
 
 @Mixin(ChickenRenderer.class)
 public class ChickenRendererMixin {
+	
 	@Inject(method = "getTextureLocation(Lnet/minecraft/world/entity/animal/Chicken;)Lnet/minecraft/resources/ResourceLocation;", at = @At("HEAD"), cancellable = true)
 	private void overrideTexture(Chicken chicken, CallbackInfoReturnable<ResourceLocation> cir) {
+		ChickenRenderer render = (ChickenRenderer) ((Object) this);
+		ChickenModel<Chicken> model = render.getModel();
+		GrabChickensModule.setRenderChickenFeetStatus(chicken, model);
+		
 		ResourceLocation loc = VariantAnimalTexturesModule.getChickenTexture(chicken);
 		if (loc != null)
 			cir.setReturnValue(loc);
