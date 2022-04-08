4fa048ccf36f5966f48b85895947a9b195b530c2
Vasco Lavos <vazkii@hotmail.com>
2022-04-08 17:20:03 +0100

    Implement vertical planks for vanilla types

--- src/main/java/vazkii/quark/content/building/module/VerticalPlanksModule.java
+++ src/main/java/vazkii/quark/content/building/module/VerticalPlanksModule.java
@@ -1,15 +1,21 @@
 package vazkii.quark.content.building.module;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.level.block.Block;
+import vazkii.quark.base.block.QuarkBlock;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.ModuleCategory;
 import vazkii.quark.base.module.QuarkModule;
+import vazkii.quark.base.util.WoodTypes;
+import vazkii.quark.base.util.WoodTypes.Wood;
 
 @LoadModule(category = ModuleCategory.BUILDING)
 public class VerticalPlanksModule extends QuarkModule {
 
 	@Override
 	public void register() {
-
+		for(Wood type : WoodTypes.VANILLA)
+			new QuarkBlock("vertical_" + type.name() + "_planks", this, CreativeModeTab.TAB_BUILDING_BLOCKS, Block.Properties.copy(type.planks()));
 	}
 	
 }
