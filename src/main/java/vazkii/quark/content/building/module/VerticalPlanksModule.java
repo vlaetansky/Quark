9eaf691f8623cfd6593d9c214a6d469efda6e3dd
Vasco Lavos <vazkii@hotmail.com>
2022-04-08 17:15:27 +0100

    Bring back vertical plank resources

---
+++ src/main/java/vazkii/quark/content/building/module/VerticalPlanksModule.java
@@ -0,0 +1,15 @@
+package vazkii.quark.content.building.module;
+
+import vazkii.quark.base.module.LoadModule;
+import vazkii.quark.base.module.ModuleCategory;
+import vazkii.quark.base.module.QuarkModule;
+
+@LoadModule(category = ModuleCategory.BUILDING)
+public class VerticalPlanksModule extends QuarkModule {
+
+	@Override
+	public void register() {
+
+	}
+	
+}
