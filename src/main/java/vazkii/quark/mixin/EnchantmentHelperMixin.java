b3e85ebb6913e47693cc88d6e4900d323c7d635a
yrsegal@gmail.com <yrsegal@gmail.com>
2022-05-10 22:11:41 -0400

    mixin for enchantmenthelper uses better form

--- src/main/java/vazkii/quark/mixin/EnchantmentHelperMixin.java
+++ src/main/java/vazkii/quark/mixin/EnchantmentHelperMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vazkii.quark.content.experimental.module.EnchantmentsBegoneModule;
 import vazkii.quark.content.tools.module.AncientTomesModule;
@@ -19,11 +18,10 @@ import java.util.Map;
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
 
-	@ModifyVariable(method = "getAvailableEnchantmentResults", at = @At("RETURN"))
-	private static List<EnchantmentInstance> begoneEnchantments(List<EnchantmentInstance> enchantmentInstances) {
-		return EnchantmentsBegoneModule.begoneEnchantmentInstances(enchantmentInstances);
+	@Inject(method = "getAvailableEnchantmentResults", at = @At("RETURN"), cancellable = true)
+	private static void begoneEnchantments(int cost, ItemStack stack, boolean treasure, CallbackInfoReturnable<List<EnchantmentInstance>> cir) {
+		cir.setReturnValue(EnchantmentsBegoneModule.begoneEnchantmentInstances(cir.getReturnValue()));
 	}
-
 
 	@Inject(method = "getEnchantments", at = @At("HEAD"), cancellable = true)
 	private static void getAncientTomeEnchantments(ItemStack stack, CallbackInfoReturnable<Map<Enchantment, Integer>> callbackInfoReturnable) {
